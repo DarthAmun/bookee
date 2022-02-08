cdc16013c8c76651ca215ef87411ab45a5e54a6d
DarthAmun <37881139+DarthAmun@users.noreply.github.com>
2022-02-08 16:43:50 +0100

    init 2

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import "./index.css";
 import "rsuite/dist/rsuite.min.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-import reportWebVitals from "./reportWebVitals";
 import { CustomProvider } from "rsuite";
 import { HashRouter } from "react-router-dom";
 
@@ -23,8 +22,3 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
